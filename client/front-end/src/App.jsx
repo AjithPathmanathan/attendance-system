fe4823e7fac677af5d437a89067ab82546213e67
pathmanathan-ajith <ajithnathan1999@outlook.com>
2024-06-01 21:54:27 +0530

    # 4.4, Redirect to the login page if the user is not logged in

--- client/front-end/src/App.jsx
+++ client/front-end/src/App.jsx
@@ -3,14 +3,14 @@ import Login from "./Login"
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
- 
+  const isAuthenticated = !!localStorage.getItem('token');
 
   return (
     
      <Router>
             <Routes>
                 <Route path="/login" element={<Login />} />
-                <Route path="/home" element={ <Home />} />
+                <Route path="/home" element={isAuthenticated ? <Home /> : <Login/>} />
                 <Route path="/" element={<Navigate to="/login" />} />
             </Routes>
         </Router>
