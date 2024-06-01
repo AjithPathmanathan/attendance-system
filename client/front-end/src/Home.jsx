bb080e3eb1f20fd577ee665cf9d6c27dace432ad
pathmanathan-ajith <ajithnathan1999@outlook.com>
2024-06-01 21:41:32 +0530

    #4.3,Redirect to the login page on logout

--- client/front-end/src/Home.jsx
+++ client/front-end/src/Home.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useNavigate } from 'react-router-dom';
 
 export const Home = () => {
+  const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate('/login');
+    };
   return (
     <div>
          <div className="vh-100 bg-dark text-white">
     <header className="d-flex justify-content-between align-items-center p-3 border-bottom border-secondary">
         <h1>Welcome to the Home Page</h1>
-        <button  className="btn btn-danger">Logout</button>
+        <button onClick={handleLogout}  className="btn btn-danger">Logout</button>
     </header>
     <main className="d-flex justify-content-center align-items-center" style={{ height: 'calc(100vh - 80px)' }}>
         <h2>Hello, User!</h2>
